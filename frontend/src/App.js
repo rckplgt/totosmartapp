8d6b6ab5f26cda5ae31aa29138eb3489edba982f
Eric <rckplgt@gmail.com>
2023-04-18 23:08:08 +0300

    Implementation of routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Container from "react-bootstrap/esm/Container";
 import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import { Container } from "react-bootstrap";
 
 const App = () => {
   return (
-    <>
-      <div className="container">
-        <Header />
-        <main className="py-3">
-          <Container>
-            <HomeScreen />
-          </Container>
-        </main>
-
-        <Footer />
-      </div>
-    </>
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/product/:id" element={<ProductScreen />} />
+          </Routes>
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 };
 
